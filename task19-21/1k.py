70f794f7b08ea06b64e0cb4c485f13180932d97f
Руслан Аитов <r_aitov@mail.ru>
2023-05-03 21:40:43 +0300

    task19-21

---
+++ task19-21/1k.py
@@ -0,0 +1,22 @@
+win = 77
+# lose = ...
+
+
+def f(a, c, m):
+    # if a > lose:
+    #     return c % 2 != m % 2
+    if a >= win:
+        return c % 2 == m % 2
+
+    if c == m:
+        return False
+    moves = [f(a + 1, c + 1, m),
+             f(a * 2, c + 1, m)]
+    return any(moves) if (c + 1) % 2 == m % 2 else all(moves)
+
+
+for a in range(1, win):
+    for m in range(1, 5):
+        if f(a, 0, m):
+            print(a, m)
+            break
---
+++ task19-21/39.py
@@ -0,0 +1,21 @@
+win = 25
+# lose = ...
+
+
+def f(a, c, m):
+    if a >= win:
+        return c % 2 == m % 2
+    # if a > lose:
+    #     return c % 2 != m % 2
+    if c == m:
+        return False
+    moves = [f(a + 2, c + 1, m),
+             f(a * 2, c + 1, m)]
+    return any(moves) if (c + 1) % 2 == m % 2 else all(moves)
+
+
+for a in range(1, win):
+    for m in range(1, 5):
+        if f(a, 0, m):
+            print(a, m)
+            break
